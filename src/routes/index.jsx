10beb43d294c3a565e95be60b38104285ec943f5
Sachin Kalekar <mr.sachinkalekar@gmail.com>
2019-06-24 21:31:58 +0530

    rename page404 to 404

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import CarsList from '../containers/list';
 import CarDetails from '../containers/details';
-import Page404 from '../components/page404';
+import Page404 from '../components/404';
 
 const routes = () =>
   <BrowserRouter>
